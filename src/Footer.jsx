db7b1f04053380b5ef9766c43f727a23c3418917
Rapha Chris <109255324+Excalibur097@users.noreply.github.com>
2025-03-07 14:53:42 +0100

    Update Footer.jsx

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -16,29 +16,29 @@ const Footer = ()=>{
         </div>
         <div className="footer-con">
           <h3>Company</h3>
-          <a>About Us</a>
-          <a>Services</a>
-          <a>Community</a>
-          <a>Testimonilas</a>
+          <a href="www.github.com">About Us</a>
+          <a href="www.github.com">Services</a>
+          <a href="www.github.com">Community</a>
+          <a href="www.github.com">Testimonilas</a>
         </div>
         <div className="footer-con">
           <h3>Support</h3>
-          <a>Help Center</a>
-          <a>Tweet @ Us</a>
-          <a>Webinars</a>
-          <a>Facebook</a>
+          <a href="www.github.com">Help Center</a>
+          <a href="www.github.com">Tweet @ Us</a>
+          <a href="www.github.com">Webinars</a>
+          <a href="www.github.com">Facebook</a>
         </div>
         <div className="footer-con">
           <h3>Links</h3>
-          <a>Courses</a>
-          <a>Become Teacher</a>
-          <a>Services</a>
-          <a>All in One</a>
+          <a href="www.github.com">Courses</a>
+          <a href="www.github.com">Become Teacher</a>
+          <a href="www.github.com">Services</a>
+          <a href="www.github.com">All in One</a>
         </div>
         <div className="footer-con">
           <h3>Contact Us</h3>
-          <a>+2348074663163</a>
-          <a>raphachris144@gmail.com</a>
+          <a href="www.github.com">+2348074663163</a>
+          <a href="www.github.com">raphachris144@gmail.com</a>
         </div>
       </div>
       <div className="bottom-footer">
